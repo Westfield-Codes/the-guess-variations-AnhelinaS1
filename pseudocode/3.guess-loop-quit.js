82661b80604aebdc157951fbf71971289e350bb2
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-03-11 14:53:50 +0000

    get ready to test

--- pseudocode/3.guess-loop-quit.js
+++ pseudocode/3.guess-loop-quit.js
@@ -1,15 +1,15 @@
 /* Simple Guess Program */
 
 // define function guessGame
-
+function guessGame(){
   // generate a random integer 1-100
-
+  let answer =  Math.floor(Math.random()*100+1);
   // display the integer for testing purposes only
-
+alert(answer);
   // create guess and set equal to 0 
-
+let guess = 0;
   // loop while guess is not equal to answer or to "q"
-
+while(guess != answer && guess !="q");
     // set guess equal to asking user to "Guess a number, 1-100"
 
     // if guess equals "q" display "Quitter!"
@@ -25,3 +25,4 @@
   // end the loop
 
 // end the function
+}
--- script.js
+++ script.js
@@ -10,18 +10,18 @@ alert(answer);
   // create guess and set equal to 0 
 let guess = 0;
   // loop while guess is not equal to answer
-while(guess!=answer)
+while(guess!=answer);{
     // set guess equal to asking user to "Guess a number, 1-100"
-  let guess = ("Guess a number, 1-100")
+guess = ("Guess a number, 1-100");
     // if guess equals answer display "Correct!" 
-  if (guess == answer) alert("Correct!")
+  if (guess == answer) alert("Correct!");
     // or if guess is greater than answer display "Too high!" 
-  else if (guess>answer) alert("Too high!" )
+  else if (guess>answer) alert("Too high!");
     // or if guess is less than answer display "Too low!" 
-  else if 
+  else if (guess<answer) alert("Too low!");
     // else say "Bad input"
-
+else alert( "Bad input");
   // end the loop
-
+}
 // end the function
 }
